7ef078a85ff59e5cc053fab12c7cc4284014296a
unknown <you@example.com>
2020-07-11 01:27:50 +0200

    therewe

--- resources/views/usersdb/users.blade.php
+++ resources/views/usersdb/users.blade.php
@@ -1,7 +1,161 @@
 <!DOCTYPE html>
 <html>
 @include('layouts.master')
-
   <!-- Main Sidebar Container -->
   @include('layouts.sidebar')
+@include('layouts.topbar')<br><br>
+
 </html>
+<div class="row mt">
+  <div class="col-lg-11">
+      <form class="form-down  style-form" action="#">
+        <div class="form-group">
+          <label class="control-label col-md-3"></label>
+          <div class="col-md-4">
+            <div class="input-group bootstrap-timepicker">
+              <input type="text" class="form-control timepicker-default">
+              <span class="input-group-btn">
+                <button class="btn btn-theme04" type="button"><i class="fa fa-search">SEARCH</i></button>
+                </span>
+            </div>
+          </div>
+        </div><br><br>
+        <section id="main-content">
+          <section class="wrapper site-min-height">
+            <div class="row mt">
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+                <div class="project-wrapper">
+                  <div class="project">
+                    <div class="photo-wrapper">
+                      <div class="photo">
+                        <a class="fancybox" href="img/portfolio/port04.jpg"><img class="img-responsive" src="img/portfolio/port04.jpg" alt=""></a>
+                      </div>
+                      <div class="overlay"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- col-lg-4 -->
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+                <div class="project-wrapper">
+                  <div class="project">
+                    <div class="photo-wrapper">
+                      <div class="photo">
+                        <a class="fancybox" href="img/portfolio/port05.jpg"><img class="img-responsive" src="img/portfolio/port05.jpg" alt=""></a>
+                      </div>
+                      <div class="overlay"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- col-lg-4 -->
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+                <div class="project-wrapper">
+                  <div class="project">
+                    <div class="photo-wrapper">
+                      <div class="photo">
+                        <a class="fancybox" href="img/portfolio/port06.jpg"><img class="img-responsive" src="img/portfolio/port06.jpg" alt=""></a>
+                      </div>
+                      <div class="overlay"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- col-lg-4 -->
+            </div>
+            <!-- /row -->
+            <div class="row mt">
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+                <div class="project-wrapper">
+                  <div class="project">
+                    <div class="photo-wrapper">
+                      <div class="photo">
+                        <a class="fancybox" href="img/portfolio/port01.jpg"><img class="img-responsive" src="img/portfolio/port01.jpg" alt=""></a>
+                      </div>
+                      <div class="overlay"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- col-lg-4 -->
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+                <div class="project-wrapper">
+                  <div class="project">
+                    <div class="photo-wrapper">
+                      <div class="photo">
+                        <a class="fancybox" href="img/portfolio/port02.jpg"><img class="img-responsive" src="img/portfolio/port02.jpg" alt=""></a>
+                      </div>
+                      <div class="overlay"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- col-lg-4 -->
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+                <div class="project-wrapper">
+                  <div class="project">
+                    <div class="photo-wrapper">
+                      <div class="photo">
+                        <a class="fancybox" href="img/portfolio/port03.jpg"><img class="img-responsive" src="img/portfolio/port03.jpg" alt=""></a>
+                      </div>
+                      <div class="overlay"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- col-lg-4 -->
+            </div>
+            <!-- /row -->
+            <div class="row mt mb">
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+                <div class="project-wrapper">
+                  <div class="project">
+                    <div class="photo-wrapper">
+                      <div class="photo">
+                        <a class="fancybox" href="img/portfolio/port04.jpg"><img class="img-responsive" src="img/portfolio/port04.jpg" alt=""></a>
+                      </div>
+                      <div class="overlay"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- col-lg-4 -->
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+                <div class="project-wrapper">
+                  <div class="project">
+                    <div class="photo-wrapper">
+                      <div class="photo">
+                        <a class="fancybox" href="img/portfolio/port05.jpg"><img class="img-responsive" src="img/portfolio/port05.jpg" alt=""></a>
+                      </div>
+                      <div class="overlay"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- col-lg-4 -->
+              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+                <div class="project-wrapper">
+                  <div class="project">
+                    <div class="photo-wrapper">
+                      <div class="photo">
+                        <a class="fancybox" href="img/portfolio/port06.jpg"><img class="img-responsive" src="img/portfolio/port06.jpg" alt=""></a>
+                      </div>
+                      <div class="overlay"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- col-lg-4 -->
+            </div>
+            <!-- /row -->
+          </section>
+          <!-- /wrapper -->
+        </section><br><br>
+        
+           
+
+
+
+          
+        
+  
