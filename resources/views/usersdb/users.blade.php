8f598a96601c3ca1e540a195e4fca908700c2a26
unknown <you@example.com>
2020-07-18 17:28:22 +0200

    thegg

--- resources/views/usersdb/users.blade.php
+++ resources/views/usersdb/users.blade.php
@@ -4,11 +4,116 @@
   <!-- Main Sidebar Container -->
   @include('layouts.sidebar')
 @include('layouts.topbar')<br><br>
-
+<!-- **********************************************************************************************************************************************************
+        MAIN SIDEBAR MENU
+        *********************************************************************************************************************************************************** -->
+    <!--sidebar start-->
+    <aside>
+      <div id="sidebar" class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu" id="nav-accordion">
+          <h5 class="centered">USERS</h5>
+          <li class="mt">
+            <a class="active" href="index.html">
+              <i class="fa fa-dashboard"></i>
+              <span>Recently Viewed</span>
+              </a>
+          </li>
+          <li class="sub-menu" href="form.html">
+            <a href="form.html">
+              <i class="fa fa-desktop"></i>
+              <span>Sell</span>
+              </a>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;">
+            <i class="fa fa-piggy-bank"></i>
+              <span>purchase History</span>
+              </a>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-cogs"></i>
+              <span>fast bine</span>
+              </a>
+            <ul class="sub">
+              <li><a href="grids.html">Grids</a></li>
+              <li><a href="calendar.html">Calendar</a></li>
+              <li><a href="gallery.html">Gallery</a></li>
+              <li><a href="todo_list.html">Todo List</a></li>
+              <li><a href="dropzone.html">Dropzone File Upload</a></li>
+              <li><a href="inline_editor.html">Inline Editor</a></li>
+              <li><a href="file_upload.html">Multiple File Upload</a></li>
+            </ul>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-money"></i>
+              <span>Help & Contacts</span>
+              </a>
+            <ul class="sub">
+              <li><a href=""></a></li>
+              <li><a href="advanced_form_components.html">Sales Transactions</a></li>
+              <li><a href="form_validation.html">Form Validation</a></li>
+              <li><a href="contactform.html">Contact Form</a></li>
+            </ul>
+          </li>
+          <li class="sub-menu">
+          <a href="javascript:;">
+            <i class="fa fa-product-hunt" aria-hidden="true"></i>
+            <span>Products</span>
+            </a>
+          <ul class="sub">
+            <li><a href="basic_table.html">Add Product</a></li>
+            <li><a href="responsive_table.html">My Product List</a></li>
+            <li><a href="advanced_table.html"></a></li>
+          </ul>
+        </li>
+        <li>
+          <li>
+            <a href="inbox.html">
+              <i class="fa fa-envelope"></i>
+              <span>Mail </span>
+              <span class="label label-theme pull-right mail-info">2</span>
+              </a>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;">
+              <i class=" fa fa-bar-chart-o"></i>
+              <span>Charts</span>
+              </a>
+            <ul class="sub">
+              <li><a href="morris.html">Morris</a></li>
+              <li><a href="chartjs.html">Chartjs</a></li>
+              <li><a href="flot_chart.html">Flot Charts</a></li>
+              <li><a href="xchart.html">xChart</a></li>
+            </ul>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;">
+              <i class="fa fa-comments-o"></i>
+              <span>Chat Room</span>
+              </a>
+            <ul class="sub">
+              <li><a href="lobby.html">Lobby</a></li>
+              <li><a href="chat_room.html"> Chat Room</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="google_maps.html">
+              <i class="fa fa-map-marker"></i>
+              <span>Google Maps </span>
+              </a>
+          </li>
+        </ul>
+        <!-- sidebar menu end-->
+      </div>
+    </aside>
+    <!--sidebar end-->
 </html>
 <div class="row mt">
   <div class="col-lg-11">
-      <form class="form-down  style-form" action="#">
+      <form class="form-down style-form" action="#">
         <div class="form-group">
           <label class="control-label col-md-3"></label>
           <div class="col-md-4">
@@ -19,139 +124,39 @@
                 </span>
             </div>
           </div>
-        </div><br><br>
-        <section id="main-content">
-          <section class="wrapper site-min-height">
-            <div class="row mt">
-              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
-                <div class="project-wrapper">
-                  <div class="project">
-                    <div class="photo-wrapper">
-                      <div class="photo">
-                        <a class="fancybox" href="img/portfolio/port04.jpg"><img class="img-responsive" src="img/portfolio/port04.jpg" alt=""></a>
-                      </div>
-                      <div class="overlay"></div>
-                    </div>
-                  </div>
-                </div>
+        </div><br><br><br>    
+        <head>
+          <meta charset="utf-2">
+          <meta name="viewport">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        </head>
+        <body> 
+         <div class="container"> 
+          <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+              <div class="item active">
+              <img class="img-responsive" src="img/portfolio/port01.jpg" alt="no image" width="550">
               </div>
-              <!-- col-lg-4 -->
-              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
-                <div class="project-wrapper">
-                  <div class="project">
-                    <div class="photo-wrapper">
-                      <div class="photo">
-                        <a class="fancybox" href="img/portfolio/port05.jpg"><img class="img-responsive" src="img/portfolio/port05.jpg" alt=""></a>
-                      </div>
-                      <div class="overlay"></div>
-                    </div>
-                  </div>
-                </div>
+              <div class="item">
+                <br><br><img class="img-responsive"src="img/portfolio/port02.jpg" alt="no image" width="550"><br>
               </div>
-              <!-- col-lg-4 -->
-              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
-                <div class="project-wrapper">
-                  <div class="project">
-                    <div class="photo-wrapper">
-                      <div class="photo">
-                        <a class="fancybox" href="img/portfolio/port06.jpg"><img class="img-responsive" src="img/portfolio/port06.jpg" alt=""></a>
-                      </div>
-                      <div class="overlay"></div>
-                    </div>
-                  </div>
-                </div>
+              <div class="item">
+                <br><br><img class="img-responsive"src="img/portfolio/port03.jpg" alt="no image" width="550"><br>
               </div>
-              <!-- col-lg-4 -->
             </div>
-            <!-- /row -->
-            <div class="row mt">
-              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
-                <div class="project-wrapper">
-                  <div class="project">
-                    <div class="photo-wrapper">
-                      <div class="photo">
-                        <a class="fancybox" href="img/portfolio/port01.jpg"><img class="img-responsive" src="img/portfolio/port01.jpg" alt=""></a>
-                      </div>
-                      <div class="overlay"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <!-- col-lg-4 -->
-              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
-                <div class="project-wrapper">
-                  <div class="project">
-                    <div class="photo-wrapper">
-                      <div class="photo">
-                        <a class="fancybox" href="img/portfolio/port02.jpg"><img class="img-responsive" src="img/portfolio/port02.jpg" alt=""></a>
-                      </div>
-                      <div class="overlay"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <!-- col-lg-4 -->
-              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
-                <div class="project-wrapper">
-                  <div class="project">
-                    <div class="photo-wrapper">
-                      <div class="photo">
-                        <a class="fancybox" href="img/portfolio/port03.jpg"><img class="img-responsive" src="img/portfolio/port03.jpg" alt=""></a>
-                      </div>
-                      <div class="overlay"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <!-- col-lg-4 -->
-            </div>
-            <!-- /row -->
-            <div class="row mt mb">
-              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
-                <div class="project-wrapper">
-                  <div class="project">
-                    <div class="photo-wrapper">
-                      <div class="photo">
-                        <a class="fancybox" href="img/portfolio/port04.jpg"><img class="img-responsive" src="img/portfolio/port04.jpg" alt=""></a>
-                      </div>
-                      <div class="overlay"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <!-- col-lg-4 -->
-              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
-                <div class="project-wrapper">
-                  <div class="project">
-                    <div class="photo-wrapper">
-                      <div class="photo">
-                        <a class="fancybox" href="img/portfolio/port05.jpg"><img class="img-responsive" src="img/portfolio/port05.jpg" alt=""></a>
-                      </div>
-                      <div class="overlay"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <!-- col-lg-4 -->
-              <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
-                <div class="project-wrapper">
-                  <div class="project">
-                    <div class="photo-wrapper">
-                      <div class="photo">
-                        <a class="fancybox" href="img/portfolio/port06.jpg"><img class="img-responsive" src="img/portfolio/port06.jpg" alt=""></a>
-                      </div>
-                      <div class="overlay"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <!-- col-lg-4 -->
-            </div>
-            <!-- /row -->
-          </section>
-          <!-- /wrapper -->
+            <!-- Left and right controls -->
+              <span class="sr-only">Previous</span>
+            </a>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+        </body>
+        </html>
         </section><br><br>
-        
+                
            
 
 
