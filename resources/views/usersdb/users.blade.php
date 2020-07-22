632cf933241d166ea1f8fc97e074517d5af33371
unknown <you@example.com>
2020-07-22 21:15:15 +0200

    thegg

--- resources/views/usersdb/users.blade.php
+++ resources/views/usersdb/users.blade.php
@@ -2,121 +2,200 @@
 <html>
 @include('layouts.master')
   <!-- Main Sidebar Container -->
-  @include('layouts.sidebar')
-@include('layouts.topbar')<br><br>
+<section id="container">
+  <!-- **********************************************************************************************************************************************************
+      TOP BAR CONTENT & NOTIFICATIONS
+      *********************************************************************************************************************************************************** -->
+  <!--header start-->
+    <!--logo start-->
+    <!--logo end-->
+      <!--  notification start -->
+        <!-- settings start -->
+          <ul class="dropdown-menu extended tasks-bar">
+            <li>
+              <p class="green">You have 4 pending tasks</p>
+            </li>
+            <li>
+              <a href="index.html#">
+                <div class="task-info">
+                  <div class="desc">Dashio Admin Panel</div>
+                  <div class="percent">40%</div>
+                </div>
+                <div class="progress progress-striped">
+                  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                    <span class="sr-only">40% Complete (success)</span>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href="index.html#">
+                <div class="task-info">
+                  <div class="desc">Database Update</div>
+                  <div class="percent">60%</div>
+                </div>
+                <div class="progress progress-striped">
+                  <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+                    <span class="sr-only">60% Complete (warning)</span>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href="index.html#">
+                <div class="task-info">
+                  <div class="desc">Product Development</div>
+                  <div class="percent">80%</div>
+                </div>
+                <div class="progress progress-striped">
+                  <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+                    <span class="sr-only">80% Complete</span>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href="index.html#">
+                <div class="task-info">
+                  <div class="desc">Payments Sent</div>
+                  <div class="percent">70%</div>
+                </div>
+                <div class="progress progress-striped">
+                  <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
+                    <span class="sr-only">70% Complete (Important)</span>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li class="external">
+              <a href="#">See All Tasks</a>
+            </li>
+          </ul>
+        </li>
+        <!-- settings end -->
+        <!-- inbox dropdown start-->
+            </a>
+          <ul class="dropdown-menu extended inbox">
+            <div class="notify-arrow notify-arrow-green"></div>
+            <li>
+            </li>
+            <li>
+              <a href="index.html#">
+                <span class="photo"><img alt="avatar" src="img/ui-zac.jpg"></span>
+                <span class="subject">
+                <span class="from">Zac Snider</span>
+                <span class="time">Just now</span>
+                </span>
+                <span class="message">
+                Hi mate, how is everything?
+                </span>
+                </a>
+            </li>
+            <li>
+              <a href="index.html#">
+                <span class="photo"><img alt="avatar" src="img/ui-divya.jpg"></span>
+                <span class="subject">
+                <span class="from">Divya Manian</span>
+                <span class="time">40 mins.</span>
+                </span>
+                <span class="message">
+                Hi, I need your help with this.
+                </span>
+                </a>
+            </li>
+            <li>
+              <a href="index.html#">
+                <span class="photo"><img alt="avatar" src="img/ui-danro.jpg"></span>
+                <span class="subject">
+                <span class="from">Dan Rogers</span>
+                <span class="time">2 hrs.</span>
+                </span>
+                <span class="message">
+                Love your new Dashboard.
+                </span>
+                </a>
+            </li>
+            <li>
+              <a href="index.html#">
+                <span class="photo"><img alt="avatar" src="img/ui-sherman.jpg"></span>
+                <span class="subject">
+                <span class="from">Dj Sherman</span>
+                <span class="time">4 hrs.</span>
+                </span>
+                <span class="message">
+                Please, answer asap.
+                </span>
+                </a>
+            </li>
+            <li>
+              <a href="index.html#">See all messages</a>
+            </li>
+          </ul>
+        </li>
+        <!-- inbox dropdown end -->
+        <!-- notification dropdown start-->
+        <li id="header_notification_bar" class="dropdown">
+          <a data-toggle="dropdown" class="dropdown-toggle" href="index.html#">
+          <ul class="dropdown-menu extended notification">
+              <a href="index.html#">
+                <span class="label label-danger"><i class="fa fa-bolt"></i></span>
+                </a>
+            </li>
+                </a>
+            </li>
+            <li>
+              <a href="index.html#">
+                <span class="label label-danger"><i class="fa fa-bolt"></i></span>
+                Disk Space Reached 85%.
+                <span class="small italic">2 hrs.</span>
+                </a>
+            </li>
+            <li>
+              <a href="index.html#">
+                <span class="label label-success"><i class="fa fa-plus"></i></span>
+                New User Registered.
+                <span class="small italic">3 hrs.</span>
+                </a>
+            </li>
+            <li>
+              <a href="index.html#">See all notifications</a>
+            </li>
+          </ul>
+        </li>
+        <!-- notification dropdown end -->
+      </ul>
+      <!--  notification end -->
+    </div>
+    <div class="top-menu">
+      <ul class="nav pull-right top-menu">
+        <li><a class="logout" href="login.html">Recently Veiw</a></li>
+        <li><a class="logout" href="login.html">Vendors Items</a></li>
+        <li><a class="logout" href="login.html">Mail</a></li>
+        <li><a class="logout" href="login.html">Purchers Histrory</a></li>
+        <li><a class="logout" href="login.html">Help & Contacts</a></li>
+        <li><a class="logout" href="login.html">Products</a></li>
+        <li><a class="logout" href="login.html">Sell</a></li>
+        <li><a class="logout" href="login.html">Fast Bine</a></li>
+        <li><a class="logout" href="login.html">Logout</a></li>
+        <li><a class="logout" href="login.html">Log in</a></li>
+        <li><a class="logout" href="login.html">Sign up</a></li> 
+      </ul>
+    </div>
+  </header>
+  <!--header end-->  <br><br>
 <!-- **********************************************************************************************************************************************************
         MAIN SIDEBAR MENU
         *********************************************************************************************************************************************************** -->
     <!--sidebar start-->
-    <aside>
-      <div id="sidebar" class="nav-collapse ">
-        <!-- sidebar menu start-->
-        <ul class="sidebar-menu" id="nav-accordion">
-          <h5 class="centered">USERS</h5>
-          <li class="mt">
-            <a class="active" href="index.html">
-              <i class="fa fa-dashboard"></i>
-              <span>Recently Viewed</span>
-              </a>
-          </li>
-          <li class="sub-menu" href="form.html">
-            <a href="form.html">
-              <i class="fa fa-desktop"></i>
-              <span>Sell</span>
-              </a>
-          </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-            <i class="fa fa-piggy-bank"></i>
-              <span>purchase History</span>
-              </a>
-          </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class="fa fa-cogs"></i>
-              <span>fast bine</span>
-              </a>
-            <ul class="sub">
-              <li><a href="grids.html">Grids</a></li>
-              <li><a href="calendar.html">Calendar</a></li>
-              <li><a href="gallery.html">Gallery</a></li>
-              <li><a href="todo_list.html">Todo List</a></li>
-              <li><a href="dropzone.html">Dropzone File Upload</a></li>
-              <li><a href="inline_editor.html">Inline Editor</a></li>
-              <li><a href="file_upload.html">Multiple File Upload</a></li>
-            </ul>
-          </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class="fa fa-money"></i>
-              <span>Help & Contacts</span>
-              </a>
-            <ul class="sub">
-              <li><a href=""></a></li>
-              <li><a href="advanced_form_components.html">Sales Transactions</a></li>
-              <li><a href="form_validation.html">Form Validation</a></li>
-              <li><a href="contactform.html">Contact Form</a></li>
-            </ul>
-          </li>
-          <li class="sub-menu">
-          <a href="javascript:;">
-            <i class="fa fa-product-hunt" aria-hidden="true"></i>
-            <span>Products</span>
-            </a>
-          <ul class="sub">
-            <li><a href="basic_table.html">Add Product</a></li>
-            <li><a href="responsive_table.html">My Product List</a></li>
-            <li><a href="advanced_table.html"></a></li>
-          </ul>
-        </li>
-        <li>
-          <li>
-            <a href="inbox.html">
-              <i class="fa fa-envelope"></i>
-              <span>Mail </span>
-              <span class="label label-theme pull-right mail-info">2</span>
-              </a>
-          </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class=" fa fa-bar-chart-o"></i>
-              <span>Charts</span>
-              </a>
-            <ul class="sub">
-              <li><a href="morris.html">Morris</a></li>
-              <li><a href="chartjs.html">Chartjs</a></li>
-              <li><a href="flot_chart.html">Flot Charts</a></li>
-              <li><a href="xchart.html">xChart</a></li>
-            </ul>
-          </li>
-          <li class="sub-menu">
-            <a href="javascript:;">
-              <i class="fa fa-comments-o"></i>
-              <span>Chat Room</span>
-              </a>
-            <ul class="sub">
-              <li><a href="lobby.html">Lobby</a></li>
-              <li><a href="chat_room.html"> Chat Room</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="google_maps.html">
-              <i class="fa fa-map-marker"></i>
-              <span>Google Maps </span>
-              </a>
-          </li>
-        </ul>
-        <!-- sidebar menu end-->
-      </div>
-    </aside>
+    
     <!--sidebar end-->
 </html>
 <div class="row mt">
-  <div class="col-lg-11">
+  <div class="col-lg-20">
       <form class="form-down style-form" action="#">
         <div class="form-group">
           <label class="control-label col-md-3"></label>
-          <div class="col-md-4">
+          <div class="col-md-7">
             <div class="input-group bootstrap-timepicker">
               <input type="text" class="form-control timepicker-default">
               <span class="input-group-btn">
@@ -124,11 +203,14 @@
                 </span>
             </div>
           </div>
-        </div><br><br><br>    
+        </div><br><br><br>  
+        
+        
         <head>
           <meta charset="utf-2">
           <meta name="viewport">
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         </head>
         <body> 
          <div class="container"> 
@@ -137,13 +219,13 @@
             <!-- Wrapper for slides -->
             <div class="carousel-inner">
               <div class="item active">
-              <img class="img-responsive" src="img/portfolio/port01.jpg" alt="no image" width="550">
+              <img class="img-responsive" src="img/portfolio/port01.jpg" alt="no image" width="10000" height="5000">
+              </div> <br><br>
+              <div class="item">
+                <br><br><img class="img-responsive"src="img/portfolio/port02.jpg" alt="no image" width="10000" height="5000"><br>
               </div>
               <div class="item">
-                <br><br><img class="img-responsive"src="img/portfolio/port02.jpg" alt="no image" width="550"><br>
-              </div>
-              <div class="item">
-                <br><br><img class="img-responsive"src="img/portfolio/port03.jpg" alt="no image" width="550"><br>
+                <br><br><img class="img-responsive"src="img/portfolio/port03.jpg" alt="no image" width="10000" height="5000"><br>
               </div>
             </div>
             <!-- Left and right controls -->
@@ -155,12 +237,18 @@
         </div>
         </body>
         </html>
-        </section><br><br>
-                
-           
-
-
-
-          
+        </section><br><br>    
+                    
+                    
+                      </section><br><br>
+                    </ul>
+                  </div>
+                </div>
+                <!-- /panel -->
+                <!-- The blueimp Gallery widget -->
+              </div>
+            </div>
+          </section>
+          <!-- /wrapper -->
+        </section>
         
-  
