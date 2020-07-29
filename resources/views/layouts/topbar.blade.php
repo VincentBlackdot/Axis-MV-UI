1b726e3907b20f322041cdcf6b32f1e17c59a2c6
VincentBlackdot <firstvincevsiame1@gmail.com>
2020-07-29 16:54:27 +0200

    upgrade admin

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -98,16 +98,19 @@
                             </li>
                         </ul>
                     </li>
+
                     <li class="nav-item dropdown nav-user">
                         <a class="nav-link nav-user-img" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="assets/images/avatar-1.jpg" alt="" class="user-avatar-md rounded-circle"></a>
                         <div class="dropdown-menu dropdown-menu-right nav-user-dropdown" aria-labelledby="navbarDropdownMenuLink2">
                             <div class="nav-user-info">
-                                <h5 class="mb-0 text-white nav-user-name">{{ Auth::user()->name }}  </h5>
+                                <h5 class="mb-0 text-white nav-user-name">{{ Auth::user()->name }}</h5>
+
                                 <span class="status"></span><span class="ml-2">Available</span>
                             </div>
-                            <a class="dropdown-item" href="#"><i class="fas fa-user mr-2"></i>Account</a>
-                            <a class="dropdown-item" href="#"><i class="fas fa-cog mr-2"></i>Setting</a>
-                            <a class="dropdown-item" href="#"><i class="fas fa-power-off mr-2"></i>Logout</a>
+                            <a class="dropdown-item" href="{{ url('/admin') }}"><i class="fas fa-user mr-2"></i>Profile</a>
+                            <a class="dropdown-item" href="{{ url('/admin') }}"><i class="fas fa-cog mr-2"></i>Setting</a>
+                            <a class="dropdown-item" ><i class="fas fa-power-off mr-2"></i>Logout</a>
+
                         </div>
                     </li>
                 </ul>
